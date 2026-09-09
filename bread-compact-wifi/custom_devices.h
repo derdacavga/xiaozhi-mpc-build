cd151554811a1fcb430932cdbbbeb097d0fff2c2
Dsn Industries <45259488+derdacavga@users.noreply.github.com>
2026-09-09 20:31:27 +0300

    Enhance DeviceController with RGB brightness control
    Added new member variables for RGB color and brightness management. Introduced setRgbBrightness method to control RGB brightness.

--- bread-compact-wifi/custom_devices.h
+++ bread-compact-wifi/custom_devices.h
@@ -7,6 +7,14 @@
 class DeviceController {
 private:
     inline static led_strip_handle_t led_strip_ = nullptr;
+    inline static int current_lamp_brightness_ = 0;
+    
+    inline static uint8_t current_r_ = 255;
+    inline static uint8_t current_g_ = 255;
+    inline static uint8_t current_b_ = 255;
+    inline static int current_rgb_brightness_ = 100;
+
+    static void updateRgbStrip();
 
 public:
     static void init();
@@ -14,6 +22,7 @@ public:
     static void setServoAngle(int angle);
     static void setLampBrightness(int percent);
     static void setRgbColor(int r, int g, int b);
+    static void setRgbBrightness(int percent);
     static void clearRgb();
 };
 
